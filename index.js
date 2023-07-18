18758d9773b2e6123407628575f0810fece202ee
Ehsan <ehsanalia555@gmail.com>
2023-07-18 15:02:06 +0500

    complete chat box with active users node js backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 40001;
 
 //New imports
 const http = require('http').Server(app);
@@ -14,15 +14,30 @@ const socketIO = require('socket.io')(http, {
 
 app.use(cors());
 
-
+let users=[];
 socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('message',(data)=>{
+    socket.on('message',data=>{
       socketIO.emit('messageResponse',data);
       // console.log(data)
-    })
+    });
+
+    //Listens when a new user joins the server
+    socket.on('newUser',data=>{
+      //Adds the new user to the list of users
+      users.push(data);
+      //send the list of users to the client
+      socketIO.emit('newUserResponse',users);
+    
+    });
     socket.on('disconnect', () => {
       console.log('🔥: A user disconnected');
+       //Updates the list of users when a user disconnects from the server
+       users=users.filter(user => user.socketID !== socket.id);
+      //  console.log(users)
+      //send the list of users to the client
+      socketIO.emit('newUserResponse',users);
+      socket.disconnect();
     });
 });
 app.get('/api', (req, res) => {
@@ -34,3 +49,12 @@ app.get('/api', (req, res) => {
 http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
